5606100aec090d92492c94f5dd66599be2bd4e2d
eediary <56810097+eediary@users.noreply.github.com>
2020-05-17 16:17:01 -0400

    Add proportionate control to current controller
    Constant current mode will adjust DAC output based on difference between target current and measured current.

--- settings.h
+++ settings.h
@@ -6,8 +6,7 @@
 #define SET_LR_TIMER_NUMBER (HAL_Timer::TIMER_TIMER1)
 #define SET_LR_TIMER_DIV (HAL_Timer::TIMER_CLK_DIV64)
 #define SET_LR_TIMER_TOP (249)
-// Current step size
-// set to 1 mA
-#define SET_LR_CUR_STEP_SIZE (0.001)
+// Current control parameters
+#define SET_LR_CUR_ERROR_SCALER (0.5)
 // Zero current calibration amount
 #define SET_LR_CAL_AMOUNT 10
