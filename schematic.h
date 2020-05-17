5047bbeeb972a9b7333a5511352dfafaa0bb1bd1
eediary <56810097+eediary@users.noreply.github.com>
2020-05-16 23:55:00 -0400

    schematic first commit

---
+++ schematic.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// System Clock Speed
+#define F_CPU (16000000UL)
+
+/********************* LOAD REGULATOR *********************/
+// SPI
+#define SCH_SPI_OP_MODE (HAL_SPI::SPI_MASTER)
+#define SCH_SPI_CLK_SEL (HAL_SPI::SPI_DIV128)
+
+// Load Regulator sampling frequency
+// Set to 1 ms
+#define SCH_LR_TIMER_NUMBER (HAL_Timer::TIMER_TIMER1)
+#define SCH_LR_TIMER_DIV (HAL_Timer::TIMER_CLK_DIV64)
+#define SCH_LR_TIMER_TOP (249)
+
+// ADS8685
+#define SCH_ADS8685_CS_PORT (HAL_GPIO::GPIO_PORTD)
+#define SCH_ADS8685_CS_PIN (HAL_GPIO::GPIO_PIN4)
+#define SCH_ADS8685_CS_DIR (HAL_GPIO::GPIO_OUTPUT)
+#define SCH_ADS8685_CS_VAL (1)
+#define SCH_ADS8685_RANGE (ADS8685::VREF_x1_25_UNIPOLAR)
+#define SCH_ADS8685_REF (ADS8685::INTERNAL_REF)
+#define SCH_ADS8685_GAIN ((4700.0+698)/698) // voltage divider = 1/8
+
+// LTC2451
+#define SCH_LTC2451_REF (5.0)
+#define SCH_LTC2451_SAMPLE_RATE (LTC2451::SPEED_60HZ)
+
+// MAX5216
+#define SCH_MAX5216_CS_PORT (HAL_GPIO::GPIO_PORTD)
+#define SCH_MAX5216_CS_PIN (HAL_GPIO::GPIO_PIN5)
+#define SCH_MAX5216_CS_DIR (HAL_GPIO::GPIO_OUTPUT)
+#define SCH_MAX5216_CS_VAL (1)
+#define SCH_MAX5216_REF (5.0)
+
+// ACS71240
+#define SCH_VOLT_PER_AMP_COEFF (0.080) // 0.080 V per Amp
+#define SCH_VOLT_TO_AMP(V, OFFSET) (((V)-(OFFSET))/(SCH_VOLT_PER_AMP_COEFF))
+#define SCH_AMP_TO_VOLT(A, OFFSET) ((A)*(SCH_VOLT_PER_AMP_COEFF)+(OFFSET))
+#define SCH_ZERO_AMP_VOLT (0) // any value below 0.5 V will cause load to draw zero current
