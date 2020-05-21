d0e7605ee8141e9bc547f09db01da7900a7c0033
eediary <56810097+eediary@users.noreply.github.com>
2020-05-20 23:20:03 -0400

    Add LED instances to Debugger module

--- Debugger.cpp
+++ Debugger.cpp
@@ -2,18 +2,24 @@
 
 Debugger::Debugger(long baud_rate, LoadRegulator &LoadRegulator_r):
 	Serial(baud_rate, SCH_UART_MODE, SET_UART_DATA_BITS, SET_UART_PARITY, SET_UART_STOP_BITS),
-	LR_r(LoadRegulator_r)
+	LR_r(LoadRegulator_r),
+	LED_G(SCH_LED_GREEN_PORT, SCH_LED_GREEN_PIN, SCH_LED_GREEN_ACTIVE),
+	LED_B(SCH_LED_BLUE_PORT, SCH_LED_BLUE_PIN, SCH_LED_BLUE_ACTIVE),
+	LED_Y(SCH_LED_YELLOW_PORT, SCH_LED_YELLOW_PIN, SCH_LED_YELLOW_ACTIVE),
+	LED_R(SCH_LED_RED_PORT, SCH_LED_RED_PIN, SCH_LED_RED_ACTIVE)
 {
 	Serial.enable_rx_int();
 	Serial.send_string("Debugger running...\r\n");
 }
 
 void Debugger::run_debugger(){
+	LED_G.on();
 	Serial.send_string("", LR_r.get_mode(), ", ");
 	Serial.send_string("", LR_r.get_measured_voltage(), 3, " V, ");
 	Serial.send_string("", LR_r.get_measured_current(), 3, " A / ");
 	Serial.send_string("", LR_r.get_control_current(), 3, " A / ");
 	Serial.send_string("", LR_r.get_target_current(), 3, " A\r\n");
+	LED_G.off();
 	
 	// Check input
 	if(Serial.read_rx_buffer(char_buffer, SET_UART_CHAR_BUFFER_SIZE)){
