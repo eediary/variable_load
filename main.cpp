4e54be609e187a1aab98b95751e19b02142bf007
eediary <56810097+eediary@users.noreply.github.com>
2020-05-23 18:23:35 -0400

    Initial commit
    Main; currently has load regulator, temperature regulator and debugger modules.

---
+++ main.cpp
@@ -0,0 +1,55 @@
+/*
+ * LoadRegulator.cpp
+ *
+ * Created: 5/7/2020 10:04:19 PM
+ * Author : jaekyung
+ */ 
+
+// AVR headers
+#include <avr/interrupt.h>
+// Project headers
+#include "schematic.h"
+#include "settings.h"
+// HAL headers
+#include "HAL_Timer.h"
+// Module headers
+#include "LoadRegulator.h"
+#include "TempRegulator.h"
+#include "Debugger.h"
+
+int main(void)
+{		
+	// Main Timer
+	HAL_Timer MainTimer = HAL_Timer(SET_MAIN_TIMER_NUMBER, SET_MAIN_TIMER_DIV, SET_MAIN_TIMER_TOP);
+	
+	// Load Regulator
+	LoadRegulator LoadRegulatorClass;
+	
+	// Temperature Regulator
+	TempRegulator TempRegulatorClass;
+	
+	// Debugger
+	#if DEBUG == 1
+	Debugger DebuggerModule = Debugger(SET_DEBUGGER_BAUD, LoadRegulatorClass, TempRegulatorClass);
+	#endif
+	
+	// Enable global interrupt
+	sei();
+    while (1) 
+    {
+		LoadRegulatorClass.regulate();
+		
+		if(MainTimer.get_int_flag()){
+			MainTimer.clear_int_flag();
+			// Run regulator
+			TempRegulatorClass.regulate();
+			
+			// Debugger
+			#if DEBUG == 1
+			DebuggerModule.run_debugger();
+			#endif
+		}
+		
+    }
+}
+
