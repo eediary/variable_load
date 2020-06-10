0db1e723600b7e1a35e3caf0c035121f22276815
eediary <56810097+eediary@users.noreply.github.com>
2020-06-09 20:24:22 -0400

    Change Debugger transmit to interrupt based
    Debugger previously used send then wait until completion, which made debugger a time consuming call. Now, transmission is interrupt based. This changes the amount of time the system spends transmitting from around 10 ms to around 0.5 ms. This also reduces program size by around 5%.

--- Debugger.cpp
+++ Debugger.cpp
@@ -28,77 +28,121 @@ void Debugger::run_debugger(){
 		float LR_measured_current = LR_r.get_measured_current();
 		float LR_control_current = LR_r.get_control_current();
 	
-		// Print message
+		// Construct string
 		LED_G.on();
-		// Message varies based on mode
 		switch(LR_r.get_mode()){
 			case(LoadRegulator::CC):
-				Serial.send_string("CC");
-				Serial.send_string(", ", LR_measured_voltage, 3, " V");
-				Serial.send_string(", ", LR_measured_current, 3, " A");
-				Serial.send_string(" / ", LR_r.get_target_current(), 3, " A");
-				Serial.send_string(" (", LR_control_current, 3, " A)");
+				// Mode
+				strcpy(char_tx_buffer, "CC, ");
+				// Measured voltage
+				append_float(LR_measured_voltage);
+				strcat(char_tx_buffer, " V, ");
+				// Measured current
+				append_float(LR_measured_current);
+				strcat(char_tx_buffer, " A, ");
+				// Target current
+				append_float(LR_r.get_target_current());
+				strcat(char_tx_buffer, " A (");
+				// Control current
+				append_float(LR_r.get_control_current());
+				strcat(char_tx_buffer, " A)");
 				break;
 			case(LoadRegulator::CP):
-				Serial.send_string("CP");
-				Serial.send_string(", ", LR_measured_voltage, 3, " V");
-				Serial.send_string(", ", LR_measured_voltage * LR_measured_current, 3, " W");
-				Serial.send_string(" / ", LR_r.get_target_power(), 3, " W");
-				Serial.send_string(" (", LR_control_current, 3, " A)");
+				// Mode
+				strcpy(char_tx_buffer, "CP, ");
+				// Measured voltage
+				append_float(LR_measured_voltage);
+				strcat(char_tx_buffer, " V, ");
+				// Measured power
+				append_float(LR_measured_voltage * LR_measured_current);
+				strcat(char_tx_buffer, " W / ");
+				// Target power
+				append_float(LR_r.get_target_power());
+				strcat(char_tx_buffer, " W (");
+				// Control current
+				append_float(LR_r.get_control_current());
+				strcat(char_tx_buffer, " A)");
 				break;
 			case(LoadRegulator::CR):
-				Serial.send_string("CR");
-				Serial.send_string(", ", LR_measured_voltage, 3, " V");
-				Serial.send_string(", ", LR_measured_voltage / LR_measured_current, 3, " R");
-				Serial.send_string(" / ", LR_r.get_target_resistance(), 3, " R");
-				Serial.send_string(" (", LR_control_current, 3, " A)");
+				// Mode
+				strcpy(char_tx_buffer, "CR, ");
+				// Measured voltage
+				append_float(LR_measured_voltage);
+				strcat(char_tx_buffer, " V, ");
+				// Measured resistance
+				append_float(LR_measured_voltage / LR_measured_current);
+				strcat(char_tx_buffer, " R / ");
+				// Target resistance
+				append_float(LR_r.get_target_resistance());
+				strcat(char_tx_buffer, " R (");
+				// Control current
+				append_float(LR_r.get_control_current());
+				strcat(char_tx_buffer, " A)");
 				break;
 			case(LoadRegulator::CV):
-				Serial.send_string("CV");
-				Serial.send_string(", ", LR_measured_voltage, 3, " V");
-				Serial.send_string(" / ", LR_r.get_target_voltage(), 3, " V");
-				Serial.send_string(", ", LR_measured_current, 3, " A");
-				Serial.send_string(" (", LR_control_current, 3, " A)");
+				// Mode
+				strcpy(char_tx_buffer, "CV, ");
+				// Measured voltage
+				append_float(LR_measured_voltage);
+				strcat(char_tx_buffer, " V / ");
+				// Target voltage
+				append_float(LR_r.get_target_voltage());
+				strcat(char_tx_buffer, " V, ");
+				// Measured current
+				append_float(LR_measured_current);
+				strcat(char_tx_buffer, " A, ");
+				// Control current
+				append_float(LR_r.get_control_current());
+				strcat(char_tx_buffer, " A)");
 				break;
 			case(LoadRegulator::OFF):
-				Serial.send_string("OFF");
-				Serial.send_string(", ", LR_measured_voltage, 3, "");
+				// Mode
+				strcpy(char_tx_buffer, "OFF, ");
+				// Measured voltage
+				append_float(LR_measured_voltage);
+				strcat(char_tx_buffer, " V");
 				break;
 		}
-		Serial.send_string(" | ", TR_r.get_duty_cycle(), "%\r\n");
+		// Duty cycle
+		strcat(char_tx_buffer, " | ");
+		append_duty_cycle();
+		strcat(char_tx_buffer, " %\r\n");
+		
+		// Transmit strings
+		Serial.send_string_int(char_tx_buffer);
 		LED_G.off();
 	
 		// Check input
-		if(Serial.read_rx_buffer(char_buffer, SET_UART_CHAR_BUFFER_SIZE)){
-			if(char_buffer[0] == 'c')
+		if(Serial.read_rx_buffer(char_rx_buffer, SET_UART_RX_BUFFER_SIZE)){
+			if(char_rx_buffer[0] == 'c')
 				// Put LR into constant current mode
 				LR_r.set_mode(LoadRegulator::CC);
-			else if(char_buffer[0] == 'p')
+			else if(char_rx_buffer[0] == 'p')
 				// Put LR into constant power mode
 				LR_r.set_mode(LoadRegulator::CP);
-			else if(char_buffer[0] == 'r')
+			else if(char_rx_buffer[0] == 'r')
 				// Put LR into constant resistance mode
 				LR_r.set_mode(LoadRegulator::CR);
-			else if(char_buffer[0] == 'v')
+			else if(char_rx_buffer[0] == 'v')
 				// Put LR into constant voltage mode
 				LR_r.set_mode(LoadRegulator::CV);
-			else if(char_buffer[0] == 'o')
+			else if(char_rx_buffer[0] == 'o')
 				// Put LR into OFF mode
 				LR_r.set_mode(LoadRegulator::OFF);
-			else if((char_buffer[0] >= 48) && (char_buffer[0] <= 57))
+			else if((char_rx_buffer[0] >= 48) && (char_rx_buffer[0] <= 57))
 				// Input is number; adjust set current, power or resistance
 				switch(LR_r.get_mode()){
 					case(LoadRegulator::CC):
-						LR_r.set_target_current(atof(char_buffer));
+						LR_r.set_target_current(atof(char_rx_buffer));
 						break;
 					case(LoadRegulator::CP):
-						LR_r.set_target_power(atof(char_buffer));
+						LR_r.set_target_power(atof(char_rx_buffer));
 						break;
 					case(LoadRegulator::CR):
-						LR_r.set_target_resistance(atof(char_buffer));
+						LR_r.set_target_resistance(atof(char_rx_buffer));
 						break;
 					case(LoadRegulator::CV):
-						LR_r.set_target_voltage(atof(char_buffer));
+						LR_r.set_target_voltage(atof(char_rx_buffer));
 						break;
 					default:
 						// do nothing
@@ -110,4 +154,18 @@ void Debugger::run_debugger(){
 				Serial.send_string("Invalid input.\r\n");
 		}
 	}
+}
+
+void Debugger::append_float(float val){
+	// appends float to tx buffer
+	char temp_buffer[SET_UART_TEMP_BUFFER_SIZE];
+	dtostrf(val, SET_DEBUGGER_FP_WIDTH, SET_DEBUGGER_FP_DECIMALS, temp_buffer);
+	strcat(char_tx_buffer, temp_buffer);
+}
+
+void Debugger::append_duty_cycle(){
+	// append temperature regulator duty cycle to tx buffer
+	char temp_buffer[SET_UART_TEMP_BUFFER_SIZE];
+	itoa(TR_r.get_duty_cycle(), temp_buffer, 10);
+	strcat(char_tx_buffer, temp_buffer);
 }
--- Debugger.h
+++ Debugger.h
@@ -1,4 +1,6 @@
 #pragma once
+// Standard headers
+#include <string.h>
 // Project headers
 #include "schematic.h"
 #include "settings.h"
@@ -15,7 +17,8 @@
 
 class Debugger{
 private:
-char char_buffer[SET_UART_CHAR_BUFFER_SIZE];
+char char_rx_buffer[SET_UART_RX_BUFFER_SIZE];
+char char_tx_buffer[SET_UART_TX_BUFFER_SIZE];
 
 // Class members
 LoadRegulator &LR_r;
@@ -28,6 +31,9 @@ LED LED_Y;
 LED LED_R;
 // Data members
 unsigned long last_time; // last time called, in ms
+// Methods
+void append_float(float val);
+void append_duty_cycle();
 
 public:
 Debugger(LoadRegulator &LoadRegulator_r, TempRegulator &TempRegulator_r, HAL_Timer &Timer_r);
